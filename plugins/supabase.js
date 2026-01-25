fd309748ad272defc95cf84dadeb45b68f140b3e
Rocio Jimenez <17301662@uttt.edu.mx>
2026-01-24 20:34:58 -0600

    Prueba deploy Netlify

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  import.meta.env.VITE_SUPABASE_https://ghehmklxrcanlxsqilpf.supabase.co ,
+  import.meta.env.VITE_SUPABASE_ANON_sb_publishable_Nmk-_w6ALvBw54tEJ7yyzQ_x47I5qAa,
 );
